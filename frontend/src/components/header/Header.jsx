923c4e314aac71ac89e57ce24074ca3ada3154bd
alievv <alievdavlat024@gmail.com>
2023-04-29 13:00:49 +0500

    frontend

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+import logo from './logo.svg'
+
+
+function Header() {
+  return (
+    <div className='container'>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+
+      <div className="modeBtn">
+        
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
